59e49f858142d6072a072ef2cf15c334fbd2894a
camilorendon <juancamilo.g.0512@gmail.com>
2022-08-31 18:30:19 -0500

    se concume la api y se muestran los pokemones con sus caracteristicas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-const Card =({pokemon, loading})=>{
+const Card =({pokemon, loading, infoPokemon})=>{
     console.log(pokemon)
     return(
         <>
@@ -9,11 +9,11 @@ const Card =({pokemon, loading})=>{
                 pokemon.map((item)=>{
                     return(
                         <>
-                        <div className="card">
-                            <h2>{item.id}</h2>
-                            <img src={item.sprites.front_default} alt=""/>
-                            <h2>{item.name}</h2>
-                        </div>
+                            <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
+                                <h2>{item.id}</h2>
+                                <img src={item.sprites.front_default} alt=""/>
+                                <h2>{item.name}</h2>
+                            </div>
                         </>
                     )
                 })
